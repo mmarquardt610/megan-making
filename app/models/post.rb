690da92e0c39dd8fd94bf6a9da41331fde3efdb9
Megan Marquardt <megan@bloc.io>
2018-11-02 13:58:34 -0700

    setup columns and attributes for post modle

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  attr_accessor :title, :content
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20181102201729) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
