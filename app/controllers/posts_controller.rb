e65bff7582a91184d5638e18e7fa01ba9c281e73
Megan Marquardt <megan@bloc.io>
2018-11-09 15:59:36 -0800

    Add a navigator for posts at bottom of index page, on show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :load_post, only: %i(show edit update destroy)
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,18 @@ class Post < ApplicationRecord
   attribute :content, :text
 
   validates_presence_of :title, :content
+
+  def previous
+    Post.where('created_at < ?', created_at)
+        .order(created_at: :desc)
+        .limit(1)
+        .last
+  end
+
+  def next
+    Post.where('created_at > ?', created_at)
+        .order(:created_at)
+        .limit(1)
+        .last
+  end
 end
